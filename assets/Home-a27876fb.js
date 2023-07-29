5569317599da8e66d8a06890e15dd3a765fdb380
Ismaiel Mahmoud <som3a1941998@gmail.com>
2023-07-29 17:01:17 +0300

    Updates

---
+++ assets/Home-a27876fb.js
@@ -0,0 +1,1 @@
+import{r as s,j as e}from"./index-dc156b48.js";import{L as o}from"./Layout-39015cb1.js";class m extends s.Component{render(){return e.jsx(o,{children:e.jsx("div",{className:"home-component",children:e.jsx("img",{src:"/images/logo.png",alt:""})})})}}export{m as default};
